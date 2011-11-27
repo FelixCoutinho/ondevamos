287aaead78f27eb426271d2059d9c7156e54a564
FelixCoutinho <felix.coutinho@gmail.com>
2011-11-26 23:32:55 -0300

    Adicionando template de votos/home

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,6 +47,8 @@ Ondevamos::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
+  config.action_mailer.raise_delivery_errors = true
+
   #Devise
   config.action_mailer.default_url_options = { :host => ENV['URL'] }
   config.action_mailer.delivery_method = :smtp
