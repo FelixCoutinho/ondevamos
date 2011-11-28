2f19614bafa4fa938767e17f0d23be4b25a2dca5
FelixCoutinho <felix.coutinho@gmail.com>
2011-11-28 16:09:01 -0300

    Correção do sign in

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Ondevamos::Application.routes.draw do
   # Definindo que a página inicial do sistema será a página inicial do
   # controller de votos.
   root :to => "votos#index"
-  
+
   namespace :usuario do
     root :to => "votos#home"
   end
-  
+
   # Configuração para que o Devise considere o recurso 'usuarios' para fazer
   # a autenticação do sistema.
   devise_for :usuarios
@@ -20,9 +20,8 @@ Ondevamos::Application.routes.draw do
   match "/votos/home" => "votos#home", :via => :get
   # Submissão para página de criação do voto
   match "/votos" => "votos#create", :via => :post
-  
+
   # Links do menu
   match "/sobre" => "menu#sobre", :via => :get
   match "/contato" => "menu#contato", :via => :get
 end
-
