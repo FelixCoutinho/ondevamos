da7b641896409a43ce83cd6a6255fe3b87b4a0e9
FelixCoutinho <felix.coutinho@gmail.com>
2011-12-12 17:35:05 -0300

    Layout

--- app/controllers/votos_controller.rb
+++ app/controllers/votos_controller.rb
@@ -14,29 +14,17 @@ class VotosController < ApplicationController
     # Resgatando usuário que está autenticado
     @voto.usuario = Usuario.find(current_usuario.id)
     @voto.data = Date.today
-    @grupo = @grupos.first
-    @restaurantes = @grupo.restaurantes
-    @jaVotou = @voto.jaVotou @voto.usuario, @grupo
+    #@grupo = @grupos.first
+    #@restaurantes = @grupo.restaurantes
+    #@jaVotou = @voto.jaVotou @voto.usuario, @grupo
     #Verificando se o usuário já votou no dia de hoje
-    if @jaVotou
-      # Google Visualizer (Gráfico da votação)
-      data_table = GoogleVisualr::DataTable.new
-      data_table.new_column('string', 'Restaurante')
-      data_table.new_column('number', 'Voto(s)')
-      @voto.contagemVotacao(@grupos.first).each do |voto|
-          # Depois da migração para Postgres foi necessário transformar o total
-          # em Integer
-          data_table.add_row([voto.label, voto.total.to_i])
-        end
-      option = { width: 527.8, height: 300 }
-      # Configurando para gráfico de estilo 'pizza'
-      @chart = GoogleVisualr::Interactive::PieChart.new(data_table, option)
-    end
+    #if @jaVotou
+    #end
   end
 
   # Adiciona/cria um voto
   def create
-    logger.info params
+    @restaurantes = Restaurante.all
     @voto = Voto.new(params[:voto])
     @grupos = Grupo.where(:usuario_id => current_usuario)
     @voto.data = Date.today
@@ -44,23 +32,24 @@ class VotosController < ApplicationController
     @voto.usuario = Usuario.find(current_usuario.id)
     # Simulando o formulário para validação do ActiveRecord
     @voto.usuario_id = current_usuario.id
-    @grupo = Grupo.where(:usuario_id => current_usuario).find(@voto.grupo)
-    @jaVotou = @voto.jaVotou @voto.usuario, @grupo
-    if @jaVotou
-      @restaurantes = Restaurante.all
-      flash[:notice] = 'Desculpe, mas você só pode ter apenas 1(um) voto por dia.'
-      redirect_to :action => "home"
-    elsif @voto.save
-      @restaurantes = Restaurante.all
-      flash[:notice] = 'Seu voto foi recebido.'
-      # Fazendo verificação de existencia de voto
-      # Foi necessário para forçar a re-checagem da tela e assim mostrar o
-      # gráfico no momento após o voto ser salvo. E não precisar recarregar
-      # a tela novamente
+    if @voto.valid?
+      @grupo = Grupo.where(:usuario_id => current_usuario).find(@voto.grupo)
       @jaVotou = @voto.jaVotou @voto.usuario, @grupo
-      redirect_to :action => "home"
+      if @jaVotou
+        flash[:notice] = 'Desculpe, mas você só pode ter apenas 1(um) voto por dia.'
+        redirect_to :action => "home"
+      elsif @voto.save
+        flash[:notice] = 'Seu voto foi recebido.'
+        # Fazendo verificação de existencia de voto
+        # Foi necessário para forçar a re-checagem da tela e assim mostrar o
+        # gráfico no momento após o voto ser salvo. E não precisar recarregar
+        # a tela novamente
+        @jaVotou = @voto.jaVotou @voto.usuario, @grupo
+        redirect_to :action => "home"
+      else
+        render :action => "home"
+      end
     else
-      @restaurantes = Restaurante.all
       render :action => "home"
     end
   end
@@ -69,11 +58,25 @@ class VotosController < ApplicationController
     @voto = Voto.new
     @grupo = Grupo.where(:usuario_id => current_usuario).find(params[:grupo_id])
     @jaVotou = @voto.jaVotou current_usuario, @grupo
-    #render :json => @jaVotou
     @voto = Voto.new
     @voto.usuario = Usuario.find(current_usuario.id)
     @voto.data = Date.today
-    @restaurantes = Grupo.find(params[:grupo_id]).restaurantes
-    render :partial => "home.html", :collection => @restaurantes, :layout => false
+    @voto.grupo = @grupo
+    if @jaVotou
+      # Google Visualizer (Gráfico da votação)
+      data_table = GoogleVisualr::DataTable.new
+      data_table.new_column('string', 'Restaurante')
+      data_table.new_column('number', 'Voto(s)')
+      @voto.contagemVotacao(@grupo).each do |voto|
+          data_table.add_row([voto.label, voto.total.to_i])
+        end
+      option = { width: 527.8, height: 300 }
+      # Configurando para gráfico de estilo 'pizza'
+      @chart = GoogleVisualr::Interactive::PieChart.new(data_table, option)
+    else
+      @grupos = Grupo.where(:usuario_id => current_usuario)
+      @restaurantes = @grupo.restaurantes
+    end
+    render :partial => "votacao.html", :layout => false
   end
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,18 +2,10 @@ $(document).ready(function(){
   $("select#grupo_id").change(function(){
     var grupo_id = $(this).find('option:selected').val();
     if(grupo_id !== ""){
-      $.getJSON('/votos/jaVotou/'+grupo_id,
-        function(response){
-          if(response !== true){
-            $.getJSON('/restaurantes/search/'+grupo_id,
-              function(response) {
-                $("div#area_votacao").html(response);
-              }
-            );
-          } else {
-
-          }
-        });
+      $('div#area_votacao').load('/votos/jaVotou/'+grupo_id).
+        ajaxStart(function() {
+          $(this).text('Triggered ajaxStart handler.');
+      });
     }
   });
 });
